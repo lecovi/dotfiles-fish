df2bfd3797dd22b7247415aad15730691acf0ba3
Leandro E. Colombo Viña <colomboleandro@bitson.com.ar>
2017-09-30 00:35:28 -0300

    Added config.fish

---
+++ config.fish
@@ -0,0 +1,14 @@
+eval (python -m virtualfish compat_aliases auto_activation)
+
+set -gx PATH $PATH ~/.local/bin
+set -x VIRTUAL_ENV_DISABLE_PROMPT 1
+set -x WORKON_HOME ~/.virtualenvs
+
+abbr -a gcam git commit -a -m
+abbr -a gcl git clone --recursive
+abbr -a sdu sudo dnf update
+abbr -a sdi sudo dnf install
+abbr -a pei pipenv install --three
+abbr -a per pipenv run
+
+eval (pipenv --completion)
