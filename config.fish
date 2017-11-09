8af3a31f00a9acce38887e2061bf7fde119a4de8
Leandro E. Colombo Viña <colomboleandro@bitson.com.ar>
2017-11-09 00:09:02 -0300

    Added spo

--- config.fish
+++ config.fish
@@ -8,7 +8,8 @@ abbr -a gcam git commit -a -m
 abbr -a gcl git clone --recursive
 abbr -a sdu sudo dnf update
 abbr -a sdi sudo dnf install
+abbr -a spo sudo poweroff
 abbr -a pei pipenv install --three
 abbr -a per pipenv run
 
-eval (pipenv --completion)
+#eval (pipenv --completion)
