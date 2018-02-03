a0145ec4825982d28c03712df86f36cce66e69b1
Leandro E. Colombo Viña <colomboleandro@bitson.com.ar>
2018-02-03 19:31:02 -0300

    Added pipenv default python interpreter & comments

--- config.fish
+++ config.fish
@@ -1,9 +1,19 @@
+## Added virtualenv support to your shell
 eval (python -m virtualfish compat_aliases auto_activation)
 
-set -gx PATH $PATH ~/.local/bin
+## Global Export PATH
+set -gx PATH $PATH ~/.local/bin /sbin
+## If you use virtualenv, you will probably need to disable the default
+## virtualenv prompt, since it doesn't play nice with fish:
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
+## Setting virtualenv directory
 set -x WORKON_HOME ~/.virtualenvs
+## Use this version of Python when creating new virtual environments
+set -x PIPENV_DEFAULT_PYTHON_VERSION 3
+## Always use fancy mode when invoking pipenv shell
+#set -x PIPENV_SHELL_FANCY 1
 
+## Abbreviations
 abbr -a gcam git commit -a -m
 abbr -a gcl git clone --recursive
 abbr -a sdu sudo dnf update
