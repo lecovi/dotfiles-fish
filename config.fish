5d617f8bf031821c75b4d90ef8b8d9a30625eba1
Leandro E. Colombo Viña <leo@bitson.group>
2018-02-19 19:36:27 -0300

    Added ANDROID config

--- config.fish
+++ config.fish
@@ -1,8 +1,11 @@
 eval (python -m virtualfish compat_aliases auto_activation)
 
-set -gx PATH $PATH ~/.local/bin
+set -gx PATH $PATH ~/.local/bin /sbin
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 set -x WORKON_HOME ~/.virtualenvs
+
+set -x ANDROID_PATH $HOME/Android/Sdk
+set -x PATH $PATH $ANDROID_HOME/tools $ANDROID_HOME/platform-tools
 
 abbr -a gcam git commit -a -m
 abbr -a gcl git clone --recursive
